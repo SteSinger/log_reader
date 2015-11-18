59776e8927b05f75e80ecfd742e363b41a719422
SteSinger <singer.stephan@googlemail.com>
2015-11-18 20:57:01 +0100

    tables work now

--- app/scripts/log/logController.js
+++ app/scripts/log/logController.js
@@ -1,65 +1,63 @@
 (function () {
-    var jetpack = require('fs-jetpack');
     var fs = require('fs');
     var readline = require('readline');
+    
+    angular.module('app').controller('logController', ['$scope', 'NgTableParams', LogController]);
 
-    angular.module('app').controller('logController', ['$scope', LogController]);
+    function LogController($scope, NgTableParams) {
+        var self = this;
+        LoadLog(process.cwd() + '/logs/test.txt', $scope, NgTableParams);
+        self.tableParams = new NgTableParams({ count: 200 }, { counts: [50, 100, 200, 500]});
+        self.logs = []
 
-    function LogController($scope) {
-        LoadLog(process.cwd() + '/logs/test.txt', $scope);
-    }
-
-
-    function LoadLog(path, $scope) {
-        var rd = readline.createInterface({
-            input: fs.createReadStream(path),
-            output: null,
-            terminal: false
-        });
-
-        var logs = [];
-
-        rd.on('line', function (line) {
-            ParseLine(line, logs)
-        });
-
-        rd.on('close', function () {
-            console.log('done');
-            $scope.logs = new NgTableParams({}, {dataset: logs});
-            //$scope.$apply();
-        });
-
-    }
-
-    function ParseLine(line, logs) {
-
-        var logLine = {};
-
-        if (line.indexOf('INFO') === 0) {
-            logLine.type = 'I';
-        } else if (line.indexOf('ERROR') === 0) {
-            logLine.type = 'E';
-        } else {
-            logLine.type = 'D';
-        }
-        
-        var dateStart = line.indexOf('[');
-        var dateEnd = line.indexOf(']'); 
-        if(dateStart > 0)
-        {
-            logLine.date = Date.parse(line.substring(dateStart + 1, dateEnd));
-        }           
-        
-        var pos = line.indexOf('-->');
-        if(pos < 0){
-            logLine.content = line;
-        } else
-        {
-            logLine.content = line.substring(pos+3).trim();
-        }
+        function LoadLog(path, $scope, NgTableParams) {
+            var rd = readline.createInterface({
+                input: fs.createReadStream(path),
+                output: null,
+                terminal: false
+            });
+    
             
-
-        logs.push(logLine);
+    
+            rd.on('line', function (line) {
+                ParseLine(line)
+            });
+    
+            rd.on('close', function () {
+                self.tableParams.settings({data: self.logs});
+            });
+    
+        }
+    
+        function ParseLine(line) {
+    
+            var logLine = {};
+    
+            if (line.indexOf('INFO') === 0) {
+                logLine.type = 'I';
+            } else if (line.indexOf('ERROR') === 0) {
+                logLine.type = 'E';
+            } else {
+                logLine.type = 'D';
+            }
+            
+            var dateStart = line.indexOf('[');
+            var dateEnd = line.indexOf(']'); 
+            if(dateStart > 0)
+            {
+                logLine.date = Date.parse(line.substring(dateStart + 1, dateEnd));
+            }           
+            
+            var pos = line.indexOf('-->');
+            if(pos < 0){
+                logLine.content = line;
+            } else
+            {
+                logLine.content = line.substring(pos+3).trim();
+            }
+                    
+            self.logs.push(logLine);
+        }
     }
 })();
     
